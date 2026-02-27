0162ff459b1db10495f9b2e07d2bdd4f3aedb608
Likesh1235 <sonysevvanam@gmail.com>
2026-02-27 16:11:26 +0530

    TrueFalse

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
 
 public class PalindromeCheckerApp {
+    public static void main(String[] args)
+    {
+       String str="madam";
+       boolean ispalindrome=true;
+       for(int i=0;i<str.length()/2;i++)
+{       if(str.charAt(i) !=str.charAt(str.length() -1 -i))
+{
+ispalindrome=false;
+break;
+}
+}
+System.out.println("Input text:"+str);
+System.out.println("\n Is it a Palindrome : True");
+}
 }
