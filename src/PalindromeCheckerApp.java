25f098336211c235e2e44e7e00b472a53c66abc5
Likesh Kanna <likeshkanna74@gmail.com>
2026-03-08 20:47:45 +0530

    UC3: Palindrome check using loop

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,64 +1,18 @@
-// UC12: Strategy Pattern Implementation
-
-import java.util.*;
-
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-
-class StackStrategy implements PalindromeStrategy {
-
-    public boolean check(String input) {
-
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
-
-class DequeStrategy implements PalindromeStrategy {
-
-    public boolean check(String input) {
-
-        Deque<Character> deque = new ArrayDeque<>();
-
-        for (char c : input.toCharArray()) {
-            deque.add(c);
-        }
-
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
-
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Input: ");
-        String input = sc.nextLine();
+        String str = "madam";
+        boolean isPalindrome = true;
 
-        PalindromeStrategy strategy = new StackStrategy();
-        boolean result = strategy.check(input);
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
 
-        System.out.println("Is Palindrome? : " + result);
-
-        sc.close();
+        System.out.println("Input text: " + str);
+        System.out.println("Is it a Palindrome: " + isPalindrome);
     }
 }
